8f09c1072dbdeb88c08efdf5df866abda2078bf0
imran-binhasan <imranbinhasan.work@gmail.com>
2025-02-15 10:56:35 +0600

    ...

--- index.js
+++ index.js
@@ -1,7 +1,55 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 const PORT = 4000;
-app.listen(PORT, ()=> {
-    console.log(`listening on port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
+
+app.get("/books", async (req, res) => {
+  try {
+    res.status(200).json({ message: "books are returned" });
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+});
+
+app.get("/books/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    res.status(200).json({ message: `unique book is returned with id:${id}` });
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+});
+
+app.post("/books/:id", async (req, res) => {
+  try {
+    const {name, description} = req.body;
+    res.status(201).json({ message: `${name} is created, info:${description}` });
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+});
+
+app.delete("/books/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    res.status(201).json({ message: `${id} is deleted`});
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+});
+
+app.put("/books/:id", async (req, res) => {
+  try {
+    const {name, description} = req.body;
+    res.status(201).json({ message: `${name} was updated, info:${description}` });
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+});
+
+// GET /books => return all the books
+// GET /books/:id => return all the books
